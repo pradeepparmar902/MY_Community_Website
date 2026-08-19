b06c0c918425cd7a1f02b720719bca9a9e8b8efc
Pradeep Parmar <pradeep.parmar@wppmedia.com>
2026-08-19 07:12:03 +0530

    Updates

--- check-deploy.js
+++
@@ -1,7 +0,0 @@
-fetch('https://pradeepparmar902.github.io/VidyaGohilTrust/index.html')
-  .then(r => r.text())
-  .then(body => {
-    const match = body.match(/src="\/VidyaGohilTrust\/assets\/index[^"]*\.js"/);
-    if(match) console.log('JS chunk:', match[0]);
-    else console.log('No match');
-  });
